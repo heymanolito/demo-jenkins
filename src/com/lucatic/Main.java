499164138e992b0ae0632cd551ce8684723d5c21
heymanolito <manuelgallardoffs@gmail.com>
2022-01-21 10:53:59 +0100

    prueba 3

--- src/com/lucatic/Main.java
+++ src/com/lucatic/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Prueba con Jenkins");
         System.out.println("Prueba con Jenkins 2");
+        System.out.println("Prueba con Jenkins 3");
     }
 }
